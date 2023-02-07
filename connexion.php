a5bd591528727cf34da0054ac3f3f0b6572a842d
Klement33 <115537505+Klement33@users.noreply.github.com>
2023-02-07 11:54:18 +0100

    Page de connexion v1

---
+++ connexion.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Connexion</title>
+</head>
+<body>
+    
+
+<h2 class="text-center">Connexion</h2>
+
+<form>
+<div class="container text-center">
+<div class="w-25 ">
+    <div class="row">
+        <div class="col align-self-center">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse e-mail">
+            <div id="emailHelp" class="form-text"></div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container text-center">
+<div class="w-25 ">
+    <div class="row">
+        <div class="col align-self-center">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container text-center">
+<div class="w-25 p-1">
+    <div class="row">
+        <div class="col align-self-center">
+            <button type="submit" class="btn btn-primary btn-lg">Se connecter</button>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container text-center">
+<div class="w-25 p-1">
+    <div class="row">
+        <div class="col align-self-center">
+            <h7>------------ ou -----------</h7>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="container text-center">
+<div class="w-25 p-1">
+    <div class="row">
+        <div class="col align-self-center">
+            <button type="submit" class="btn btn-primary btn-lg">Crée un compte</button>
+        </div>
+    </div>
+</div>
+</div>
+</form>
+
+</body>
+</html>
