b31150af88e3af75a93afa9ad2b06077e915abd6
asstriiiiid <116190444+asstriiiiid@users.noreply.github.com>
2023-02-08 17:02:21 +0100

    Add files via upload

--- mentionslegales.php
+++ mentionslegales.php
@@ -1,1 +1,76 @@
-<?php require_once 'header.php';
+<?php require_once 'header.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+
+            <h1 > MENTIONS LÉGALES </h1>
+    <p> 
+Mentions légales publiées le 10 Février 2023
+</p>
+<h3> DIRECTEUR DE PUBLICATION </h3> 
+<p> Le palais bordelais est un blog édité par Joséphine SAINT-YGNAN, Clément DIAZ, Bryan GONCALVES , Pablo CASOLA et Astrid CHAPRON.</p>
+<h3> RAISON SOCIALE DE L'HÉBERGEUR </h3>
+<p> Dirigé par Alban Pichon<br>
+IUT Bordeaux Montaigne <br>
+1 rue jacques ellul <br>
+33080 Bordeaux Cedex France <br>
+Tél: 05 57 12 20 44
+</p>
+<h3> PROPRIÉTÉ INTELLECTUELLE </h3>
+<p> L’ensemble des éléments présents sur ce site web sont la propriété exclusive de “Le Palais Bordelais” cela inclut textes ,savoir-faire ainsi que tous les éléments composant le site. Toutes utilisations partielles ou totales sont strictement interdites si il n’ y a pas eu d’accord préalable avec les éditeurs des articles ou propriétaires du site web Le Palais Bordelais.  </p>
+<h3> DROITS D’AUTEUR </h3>
+<p> Nous ne sommes en aucun cas et à aucun moment responsable de l’utilisation des informations, quelles que soient les conséquences qu’elles engendrent. 
+Peu importe la raison, il est strictement interdit de se représenter totalement ou partiellement comme “Le Palais Bordelais”, cela représente un violation de la loi, d’après l’article L. 335-2 et suivants du Code de la propriété intellectuelle.</p>
+<h3> UTILISATION DES COOKIES </h3>
+    <p> 
+Le Palais Bordelais contient des cookies qui sont des petits fichiers stockés par un serveur dans le terminal (ordinateur, téléphone, etc.) , 
+L'utilisateur à la possibilité de les refuser aux risques de se voir l'accès au blog perturbé ou bloqué car cela limite les utilisations et certaines possibilités présentes sur le site web.
+</p>
+<h3> RESPONSABILITÉ </h3>
+<p>Le Palais Bordelais n’est pas responsable d’un quelconque problème technique qui peut être présent sur le site ou dans les téléchargements liés à celui-ci. Que ce soit lors de la connexion au site web ou lors de l’acheminement vers des liens externes au site, seuls les hébergeurs des sites sont responsables des problèmes techniques qui peuvent être rencontrés.
+Le site web s’applique à fournir des informations aussi précises que possible il ne pourra lui en tenir rigueur en cas d’incertitudes ou de retard de mises à jour. 
+Les informations regroupées sur le site sont à titre indicatif et peuvent être modifiées à leur date de publication.
+</p>
+<h3> AVERTISSEMENT GÉNÉRAL </h3>
+<p> L’ensemble des éléments présents sur ce site web sont mis à disposition des internautes gratuitement et sans aucune garantie. Ces éléments et informations  sont sous l'entière  responsabilité des internautes. 
+Toutes les informations, textes et autres disponibles sur le site web, proviennent de sources extérieures, site web, livre, article… Nous ne garantissons pas qu’elles soient exactes, certaines informations peuvent être sources d'erreurs, contenir des fautes ou être inexactes. Ils est aussi possible qu’elle ne soit plus à jour sur l’actualité.
+</p>
+<h3> POLITIQUE DE CONFIDENTIALITÉ </h3>
+<p> Notre site web récolte des données, celles-ci ne sont  aucunement revendus ou cédées à des entreprises ou organismes tiers 
+Aucune adresse mail , mot de passe et autres informations personnelles  ne sera transmise à autrui collaborateur et partenaires compris .
+</p>
+<h3> COPYRIGHT ET CRÉDITS </h3>
+    <p> Lorsque notre site web utilise des images ou visuels présentant des crédits ou copyright, cela est précisé dans l’article qu’elles viennent d’un professionnel ou d’un particulier.
+Sites webs externes d'où provient certain contenu présent sur le blog : 
+</p>
+            </div>
+        </div>
+    </div>
+
+
+<style>
+    h1{
+        text-align: center;
+        margin: 50px 0px;
+        font-family: 'Alice', serif;
+        
+    }
+    h3{
+        
+        color: rgb(92, 25, 25);
+        text-align: center;
+        margin: 30px 0px;
+        font-family: 'Alice', serif;
+    }
+    p{
+        text-align: center;
+        font-family:'roboto', serif;
+    }
+    </style>
+
